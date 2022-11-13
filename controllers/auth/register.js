beec1776d6d2ea483ca4c00a060bf7bfbc95fa7f
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2022-11-14 00:07:08 +0200

    adds the ability to change the avatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
@@ -12,7 +13,12 @@ const register = async (req, res) => {
     throw RequestError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  const result = await User.create({ email, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({
     email: result.email,
   });
